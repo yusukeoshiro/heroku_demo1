7936513e633906d8887079a8adc150b62674388b
Yusuke Oshiro <yusuke@oshiro1.com>
2015-11-02 01:38:35 +0900

    a

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,15 @@
 class WelcomeController < ApplicationController
+	def index
+		require 'pg'
+		#conn = PG::Connection.open(:dbname => 'myapp_development')
+		conn = PG::Connection.open('postgres://jlvqgelfdubvrg:6lZ-mGpJwbPffz62kXQvXyHOvD@ec2-54-225-194-162.compute-1.amazonaws.com:5432/d3qroctqca1n9g')
+		@res = conn.exec_params('SELECT * from salesforce.contact')
+		#res[0]
+
+
+	end
+
+
+
+	
 end
