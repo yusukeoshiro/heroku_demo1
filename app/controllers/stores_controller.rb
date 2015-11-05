f0a07548e9194f283cd51e812fb8619db5e5343b
Yusuke Oshiro <yusuke@oshiro1.com>
2015-11-05 16:14:10 +0900

    map app

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,6 +1,11 @@
 class StoresController < ApplicationController
 
 	def index
+
+        conn = PG::Connection.open(ENV['DATABASE_URL'])
+		@res = conn.exec_params('SELECT * from salesforce.store__c')
+		conn.close
+
 	end
 
 end
