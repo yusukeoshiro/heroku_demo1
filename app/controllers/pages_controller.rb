de3b1b96648c9d8080b2fc99c9b04c66d733aa88
Yusuke Oshiro <yusuke@oshiro1.com>
2015-11-14 10:55:14 +0900

    point added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,13 @@ class PagesController < ApplicationController
 
 	def userid
 		#@user = User.find(session['user_id'])
+		query = "SELECT * FROM salesforce.contact WHERE sfid = '#{session['user_id']}'"
+		puts query
+		conn = PG::Connection.open(ENV['DATABASE_URL'])
+		res = conn.exec_params(query)
+		conn.close
+
+		@user = res[0]
 	end
 
 end
