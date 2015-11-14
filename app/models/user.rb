b61d115e208df7038fc3db6dedd4d778a886bf04
Yusuke Oshiro <yusuke@oshiro1.com>
2015-11-14 10:33:30 +0900

    password field change

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
       #find_by_email_and_password(email, Digest::SHA1.hexdigest(password))
       conn = PG::Connection.open(ENV['DATABASE_URL'])
       #@res = conn.exec_params("SELECT * FROM salesforce.contact (lastname, password__c, email) VALUES ('#{@user.name}','#{@user.password}','#{@user.email}')")
-      res = conn.exec_params("SELECT * FROM salesforce.contact WHERE email = '#{email}' AND password__c = '#{Digest::SHA1.hexdigest(password)}'")
+      res = conn.exec_params("SELECT * FROM salesforce.contact WHERE email = '#{email}' AND hd_password__c = '#{Digest::SHA1.hexdigest(password)}'")
       conn.close
       if res.num_tuples() > 0
       	return res[0]
